75dd1c18c9d4d8e5dcffad19b0bba9b6d4953fbb
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-10-24 14:33:13 -0500

    Initial repo setup

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import NewsStories from './components/NewsStories.vue';
+
+</script>
+
+<template>
+  <header>
+    <h1>Hacker News: Uncommented</h1>
+  </header>
+  
+  <main>
+    <NewsStories />
+  </main>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+h1 {
+  font-size: 2rem;
+  font-weight: 700;
+  margin: auto;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/NewsStories.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+    data() {
+        return {
+            stories: [],
+            max_score: 0
+        };
+    },
+    mounted() {
+         this.fetchTopStories();
+    },
+    methods: {
+        async fetchTopStories() {
+            try {
+                const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+                const storyIds = await response.json();
+                const top10Ids = storyIds.slice(0, 30);
+
+                const storyPromises = top10Ids.map(id =>
+                    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(response => response.json())
+                );
+
+                this.stories = await Promise.all(storyPromises);
+                this.max_score = Math.max(...this.stories.map(story => story.score));
+
+            } catch (error) {
+                console.error("An error occurred while fetching data:", error);
+            }
+        },
+        paddedScore(score) {
+            const scoreString = score.toString();
+            const maxScoreString = this.max_score.toString();
+            const maxScoreStringLength = maxScoreString.length;
+            const scoreStringLength = scoreString.length;
+            const paddingLength = maxScoreStringLength - scoreStringLength;
+            const padding = " ".repeat(paddingLength);
+            return `${padding}${scoreString}`;
+        },
+        getDomain(url) {
+            const urlObject = new URL(url);
+            return urlObject.hostname;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="news-stories">
+        <ul>
+            <li v-for="story in stories.sort((a, b) => b.score - a.score)" :key="story.id">
+                <a :href="story.url" target="_blank" rel="noopener">[{{ this.paddedScore(story.score) }}] {{ story.title }} </a>
+                <span> {{ getDomain(story.url) }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+span {
+    margin-left: 1rem;
+    color: #828282;
+    font-size: 0.8rem;
+}
+a {
+    white-space: pre;
+}
+ul {
+    list-style: none;
+    /* padding: 0; */
+}
+</style>
